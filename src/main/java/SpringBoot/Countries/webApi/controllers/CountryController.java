cc459d92e7b3b706c0fbccad25cd8048e800734c
Faruk <47918693+farukozelll@users.noreply.github.com>
2023-03-26 19:01:54 +0300

    Update CountryController.java

--- src/main/java/SpringBoot/Countries/webApi/controllers/CountryController.java
+++ src/main/java/SpringBoot/Countries/webApi/controllers/CountryController.java
@@ -10,8 +10,8 @@ import java.util.List;
 import java.util.Map;
 
 @RestController
-@RequestMapping("/api/countries") // controller /countries kök dizini altında HTTP isteklerine yanıt verir
-public class CountryController { //bu sınıf HTTP isteklerini işleyerek CountryService bileşeninin yöntemlerini çağırır ve sonuçları HTTP yanıtı olarak döndürür
+@RequestMapping("/api/countries") 
+public class CountryController { 
 
     private final CountryServices countryServices;
 
@@ -20,25 +20,25 @@ public class CountryController { //bu sınıf HTTP isteklerini işleyerek Countr
         this.countryServices = countryServices;
     }
 
-    @GetMapping("/getCountries") //HTTP GET isteklerine yanıt veren yöntem
+    @GetMapping("/getCountries") 
     public Map<String, Country>  getAllCountries() {
         return countryServices.getAllCountries();
     }
 
-    @GetMapping("/{code}") //belirli bir ülkenin koduna (code) göre veritabanından alır
-    public Country getCountryByCode(@PathVariable String code) { //pathVariable  HTTP isteğinin yoluna erişim sağlar
+    @GetMapping("/{code}") 
+    public Country getCountryByCode(@PathVariable String code) { 
         return countryServices.getCountryByCode(code);
     }
 
     @GetMapping("/sorted")//@RequestParam default değer atamak istenirse defaultValue özelliğini
-    public List<Country> getCountriesSortedByPhoneCode(@RequestParam(required = true) boolean ascending) {//veritabanındaki ülkeleri telefon kodlarına göre sıralar ve isteğe bağlı olarak artan veya azalan sıralama yapar. RequestParam HTTP isteğinin sorgu parametrelerine erişim sağlar
+    public List<Country> getCountriesSortedByPhoneCode(@RequestParam(required = true) boolean ascending) {
         return countryServices.getCountriesSortedByPhone(ascending);
     }
     /*  @GetMapping("/countries/by-phone-code/{phoneCode}")
     public List<Country> getCountriesByPhoneCode(@PathVariable String phoneCode) {
         return countryServices.getCountriesByPhoneCodeOrderByPhoneCodeAsc(phoneCode);
     }*/
-    @GetMapping("/filter") //@RequestParam parametresini isteğe bağlı yapmak istenirse required özelliğini false yapılır
+    @GetMapping("/filter")
     public List<Country> getCountriesByProperties(@RequestParam(required = false) String currency,
                                                   @RequestParam(required = false) String phone,
                                                   @RequestParam(required = false) String continent) {
